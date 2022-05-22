f1c4f7db4ef1d5e7c466bdfe6d772097b9c3b72f
Harry Chen <harrychen0314@gmail.com>
2022-05-22 21:23:13 +0000

    add language config

---
+++ judger/configs/languages.ts
@@ -0,0 +1,33 @@
+enum SubmissionLang {
+  C,
+  CPP,
+}
+
+interface SubmissionConfig {
+  srcFile: string;
+  binaryFile: string;
+  displayName: string;
+  compileCommand: string;
+  executeCommand: string;
+}
+
+const languageConfigs: Record<SubmissionLang, SubmissionConfig> = {
+  [SubmissionLang.CPP]: {
+    srcFile: 'program.cpp',
+    binaryFile: 'a.out',
+    displayName: 'C++',
+    compileCommand:
+      '/usr/bin/g++ -o2 -w -fmax-errors=3 -std=c++17 {src_path} -lm -o {binary_path}',
+    executeCommand: '{exe_path}',
+  },
+  [SubmissionLang.C]: {
+    srcFile: 'program.c',
+    binaryFile: 'a.out',
+    displayName: 'C',
+    compileCommand:
+      '/usr/bin/gcc -o2 -w -fmax-errors=3 -std=c11 {src_path} -lm -o {binary_path}',
+    executeCommand: '{exe_path}',
+  },
+};
+
+export {SubmissionLang, languageConfigs};
