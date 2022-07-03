062a8a320e604be4bc3d45b9790add358b8f857c
Harry Chen <harrychen0314@gmail.com>
2022-07-03 03:53:16 +0000

    implement judger webhook

--- packages/judger/src/start.ts
+++ packages/judger/src/start.ts
@@ -2,50 +2,75 @@ import { destroySandbox, initSandbox } from './services/sandbox.service'
 import { gradeSubmission } from './services/grade.service'
 import { compileSubmission } from './services/compile.service'
 
-import { messageReceiver, GradeTask, CompileTask, JudgerTaskType } from '@project-carbon/shared'
-import os = require('os')
+import { messageReceiver, GradingTask, CompilingTask, JudgerTaskType } from '@project-carbon/shared'
 
-const sandboxes = new Set()
+import * as os from 'os'
+import { randomUUID } from 'crypto'
+import got from 'got'
 
-async function handleGradeTask (task: GradeTask, box: number): Promise<void> {
-  await initSandbox(box)
-  const result = await gradeSubmission(task, box)
-  console.log(result)
-  await destroySandbox(box)
-  sandboxes.add(box)
+const availableBoxes = new Set()
+const judgerID = randomUUID()
+const serverBaseURL = process.env.SERVER_BASE_URL ?? '127.0.0.1:3000'
+
+async function handleGradingTask (task: GradingTask, boxID: number): Promise<void> {
+  await initSandbox(boxID)
+  const result = await gradeSubmission(task, boxID)
+  await destroySandbox(boxID)
+  availableBoxes.add(boxID)
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  await got.put(new URL(`/submissions/${task.submissionID}/gradingResult/${task.testcaseIndex}`, serverBaseURL).href, {
+    json: result,
+    timeout: {
+      request: 20000
+    }
+  })
 }
 
-async function handleCompileTask (task: CompileTask, box: number): Promise<void> {
-  await initSandbox(box)
-  const result = await compileSubmission(task, box)
-  console.log(result)
-  await destroySandbox(box)
-  sandboxes.add(box)
+async function handleCompilingTask (task: CompilingTask, boxID: number): Promise<void> {
+  await initSandbox(boxID)
+  const result = await compileSubmission(task, boxID)
+  await destroySandbox(boxID)
+  availableBoxes.add(boxID)
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  await got.put(new URL(`/submissions/${task.submissionID}/compilingResult`, serverBaseURL).href, {
+    json: result,
+    timeout: {
+      request: 20000
+    }
+  })
 }
 
 export async function startJudger (): Promise<void> {
+  console.log(`Starting Judger ${judgerID}.`)
+
   const cores = os.cpus().length
-  const destroyQueue: Array<Promise<{ box: number }>> = []
+
+  console.log(`${cores} CPU cores detected.`)
+
+  const destroyQueue: Array<Promise<{ boxID: number }>> = []
   for (let id = 1; id <= cores; id += 1) {
     destroyQueue.push(destroySandbox(id))
-    sandboxes.add(id)
+    availableBoxes.add(id)
   }
   await Promise.all(destroyQueue)
 
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    const messages = await messageReceiver.receiveMessages(sandboxes.size)
-    const tasks: Array<GradeTask | CompileTask> = messages.map(
-      message => message.body
-    )
-    tasks.forEach(task => {
-      const box = sandboxes.values().next().value
-      sandboxes.delete(box)
-      if (task.type === JudgerTaskType.Grade) {
-        void handleGradeTask(task, box)
-      } else if (task.type === JudgerTaskType.Compile) {
-        void handleCompileTask(task, box)
+  console.log('All sandboxes initialized.')
+
+  messageReceiver.subscribe({
+    processMessage: async (message): Promise<void> => {
+      const task = message.body
+      const boxID = availableBoxes.values().next().value
+      availableBoxes.delete(boxID)
+      if (task.type === JudgerTaskType.Grading) {
+        void handleGradingTask(task, boxID)
+      } else if (task.type === JudgerTaskType.Compiling) {
+        void handleCompilingTask(task, boxID)
       }
-    })
-  }
+    },
+    processError: async (error): Promise<void> => {
+      console.error(error)
+    }
+  })
+
+  console.log('Subscribed to Service Bus. Receiving tasks.')
 }
