e29fcf820f5f64811229e0ec9be324e0bd8c1747
Harry Chen <harrychen0314@gmail.com>
2022-07-06 01:48:47 +0000

    fix testcase upload

--- packages/server/src/services/testcases.service.ts
+++ packages/server/src/services/testcases.service.ts
@@ -1,18 +1,15 @@
 import { randomUUID } from 'crypto'
 import { Worker } from 'node:worker_threads'
 
-import { MultipartFile } from '@fastify/multipart'
-
 import { deleteBlob } from '@project-carbon/shared'
 
 import path = require('path')
 
-export async function uploadTestcase (testcase: MultipartFile): Promise<{testcaseID: string}> {
+export async function uploadTestcase (testcasePath: string): Promise<{testcaseID: string}> {
   const testcaseID = randomUUID()
-  const data = (await testcase.toBuffer()).toString()
   return await new Promise((resolve, reject) => {
     const worker = new Worker(path.join(__dirname, '../workers/uploadTestcase.worker.js'), {
-      workerData: { data, testcaseID }
+      workerData: { testcasePath, testcaseID }
     })
     worker.on('message', resolve)
     worker.on('error', reject)
@@ -25,6 +22,6 @@ export async function uploadTestcase (testcase: MultipartFile): Promise<{testcas
 }
 
 export async function deleteTestcase (testcaseID: string): Promise<{testcaseID: string}> {
-  const deletedBlob = await deleteBlob({ containerName: 'testcases', blobName: testcaseID })
-  return { testcaseID: deletedBlob.containerName }
+  await deleteBlob({ containerName: 'testcases', blobName: testcaseID })
+  return { testcaseID: testcaseID }
 }
