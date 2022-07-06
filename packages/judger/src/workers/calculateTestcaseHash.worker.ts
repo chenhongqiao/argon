025636f85e71027c1d316413d47d1dc4d14f23d6
Harry Chen <harrychen0314@gmail.com>
2022-07-06 01:46:08 +0000

    add calculateTestcaseHash worker

---
+++ packages/judger/src/workers/calculateTestcaseHash.worker.ts
@@ -0,0 +1,15 @@
+import { parentPort, workerData } from 'node:worker_threads'
+
+import { cleanTestcase, readFile } from '@project-carbon/shared'
+
+import { createHash } from 'crypto'
+
+const { testcasePath } = workerData
+readFile(testcasePath).then((result) => {
+  const cleaned = cleanTestcase(result.data)
+  const hash = createHash('md5').update(cleaned).digest('base64')
+  console.log(hash)
+  parentPort?.postMessage({ md5: hash })
+}).catch((err) => {
+  throw err
+})
