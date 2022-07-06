c12855a7e41908b86744d47b3de3484fdcca2e08
Harry Chen <harrychen0314@gmail.com>
2022-07-06 01:40:56 +0000

    add makeExecutable

--- packages/judger/src/utils/system.util.ts
+++ packages/judger/src/utils/system.util.ts
@@ -2,3 +2,7 @@ import cp = require('child_process')
 import util = require('util')
 
 export const exec = util.promisify(cp.exec)
+
+export async function makeExecutable (path: string): Promise<void> {
+  await exec(`chmod +x ${path}`)
+}
