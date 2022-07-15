b54a305e4b9fbb910df13fd9e5f4eaa2e4ef51d3
Harry Chen <harrychen0314@gmail.com>
2022-07-15 06:23:44 +0000

    implement user register route
    Signed-off-by: Harry Chen <harrychen0314@gmail.com>

---
+++ packages/server/src/routes/users.route.ts
@@ -0,0 +1,38 @@
+import { FastifyPluginCallback } from 'fastify'
+
+import { registerUser } from '../services/users.service'
+
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
+
+import { Type } from '@sinclair/typebox'
+import { ConflictError, NewUserSchema } from '@chenhongqiao/carbon-common'
+
+export const usersRoutes: FastifyPluginCallback = (app, options, done) => {
+  const route = app.withTypeProvider<TypeBoxTypeProvider>()
+  route.post(
+    '/',
+    {
+      schema: {
+        body: NewUserSchema,
+        response: {
+          201: Type.Object({ userID: Type.String() }),
+          409: Type.Object({ message: Type.String() })
+        }
+      }
+    },
+    async (request, reply) => {
+      const user = request.body
+      try {
+        const result = await registerUser(user)
+        void reply.status(201).send(result)
+      } catch (err) {
+        if (err instanceof ConflictError) {
+          void reply.status(409).send({ message: err.message })
+        } else {
+          throw err
+        }
+      }
+    }
+  )
+  done()
+}
