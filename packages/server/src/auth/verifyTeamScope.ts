1a38de2bd970539661ab1579961dda629f9d3d94
Harry Chen <harrychen0314@gmail.com>
2022-10-14 05:00:11 +0000

    implement scope-based authorization
    Signed-off-by: Harry Chen <harrychen0314@gmail.com>

---
+++ packages/server/src/auth/verifyTeamScope.ts
@@ -0,0 +1,18 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export default function verifyTeamScope (scopes: string[]) {
+  return function handler (request: FastifyRequest, reply: FastifyReply, done) {
+    // @ts-expect-error: url of domain resources always includes domainId as a parameter
+    const domainId = request.params.domainId
+    if (domainId == null || typeof domainId !== 'string') {
+      return done(new Error('Resource not associated with a domain.'))
+    }
+    scopes.forEach((scope) => {
+      if (!request.user.scopes[domainId].includes(scope)) {
+        return done(new Error('You are not allowed to perform this action.'))
+      }
+    })
+
+    return done()
+  }
+}
