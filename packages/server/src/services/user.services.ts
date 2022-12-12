a59c862918657d38376644d43607b0c15671a62d
Harry Chen <harrychen0314@gmail.com>
2022-12-12 04:52:30 +0000

    feat: rewrite user services to use mongo
    Signed-off-by: Harry Chen <harrychen0314@gmail.com>

--- packages/server/src/services/user.services.ts
+++ packages/server/src/services/user.services.ts
@@ -1,6 +1,6 @@
-import { User, NewUser, ConflictError, AzureError, NotFoundError, AuthenticationError, AuthorizationError, UserRole } from '@argoncs/types'
-import { CosmosDB } from '@argoncs/libraries'
-import { randomUUID, randomBytes, pbkdf2 } from 'node:crypto'
+import { User, NewUser, NotFoundError, AuthenticationError, AuthorizationError, UserRole, ConflictError, UserDB } from '@argoncs/types'
+import { mongoDB, MongoServerError } from '@argoncs/libraries'
+import { randomBytes, pbkdf2 } from 'node:crypto'
 
 import { promisify } from 'node:util'
 
@@ -9,21 +9,13 @@ import { emailClient } from '../connections/email.connections'
 const randomBytesAsync = promisify(randomBytes)
 const pbkdf2Async = promisify(pbkdf2)
 
-const usersContainer = CosmosDB.container('users')
-const usernameIndexContainer = CosmosDB.container('usernameIndex')
-const emailIndexContainer = CosmosDB.container('emailIndex')
-const verificationsContainer = CosmosDB.container('emailVerifications')
+const userCollection = mongoDB.collection<UserDB>('users')
+const verificationCollection = mongoDB.collection('emailVerifications')
 
-interface UserIndex {
-  id: string
-  userId: string
-}
-
-export async function registerUser (newUser: NewUser): Promise<{userId: string, email: string}> {
+export async function registerUser (newUser: NewUser): Promise<{ userId: string, email: string }> {
   const salt = (await randomBytesAsync(32)).toString('base64')
   const hash = (await pbkdf2Async(newUser.password, salt, 100000, 512, 'sha512')).toString('base64')
-  const userId = randomUUID()
-  const user: User = {
+  const user: Omit<User, '_id'> = {
     name: newUser.name,
     email: newUser.email,
     password: {
@@ -32,166 +24,103 @@ export async function registerUser (newUser: NewUser): Promise<{userId: string, 
     },
     role: UserRole.User,
     verifiedEmail: null,
-    id: userId,
     username: newUser.username,
     scopes: {}
   }
-  const usernameIndex: UserIndex = {
-    id: user.username,
-    userId: userId
-  }
-  const emailIndex: UserIndex = {
-    id: user.email,
-    userId: userId
-  }
 
   try {
-    await usernameIndexContainer.items.create(usernameIndex)
+    const { insertedId } = await userCollection.insertOne(user)
+    return { userId: insertedId.toString(), email: user.email }
   } catch (err) {
-    if (err.code === 409) {
-      throw new ConflictError('Username exists.', { user })
+    if (err instanceof MongoServerError && err.code === 11000 && err.keyValue != null) {
+      if (err.keyValue.email !== undefined) {
+        throw new ConflictError('Email taken by another user.', { user })
+      } else if (err.keyValue.username !== undefined) {
+        throw new ConflictError('Username taken by another user.', { user })
+      } else {
+        throw err
+      }
     } else {
       throw err
     }
   }
-
-  try {
-    await emailIndexContainer.items.create(emailIndex)
-  } catch (err) {
-    if (err.code === 409) {
-      await usernameIndexContainer.item(user.username, user.username).delete()
-      throw new ConflictError('Email exists.', { user })
-    } else {
-      throw err
-    }
-  }
-
-  const created = await usersContainer.items.create(user)
-  if (created.resource == null) {
-    await usernameIndexContainer.item(user.username, user.username).delete().catch()
-    await emailIndexContainer.item(user.email, user.email).delete().catch()
-    throw new AzureError('No resource ID returned when creating user.', created)
-  }
-  return { userId: created.resource.id, email: user.email }
 }
 
 export async function fetchUser (userId: string): Promise<User> {
-  const userItem = usersContainer.item(userId, userId)
-  const fetched = await userItem.read<User>()
-  if (fetched.resource != null) {
-    return fetched.resource
-  } if (fetched.statusCode === 404) {
+  const user = await userCollection.findOne({ _id: userId })
+  if (user == null) {
     throw new NotFoundError('User not found.', { userId })
-  } else {
-    throw new AzureError('Unexpected CosmosDB return.', fetched)
   }
+  return user
 }
 
 export async function userIdExists (userId: string): Promise<boolean> {
-  const userItem = usersContainer.item(userId, userId)
-  const fetched = await userItem.read<User>()
-  if (fetched.resource != null) {
-    return true
-  } if (fetched.statusCode === 404) {
-    return false
-  } else {
-    throw new AzureError('Unexpected CosmosDB return.', fetched)
-  }
+  return Boolean(userCollection.countDocuments({ _id: userId }))
 }
 
 export async function updateUser (user: User, userId: string): Promise<{ userId: string }> {
-  const userWithId = { ...user, userId }
-  const userItem = usersContainer.item(userId, userId)
-  const updated = await userItem.replace(userWithId)
-  if (updated.resource != null) {
-    return { userId: updated.resource.id }
-  } if (updated.statusCode === 404) {
+  const { upsertedId, matchedCount } = await userCollection.updateOne({ _id: userId }, { $set: user })
+  if (matchedCount === 0) {
     throw new NotFoundError('User not found.', { userId })
-  } else {
-    throw new AzureError('Unexpected CosmosDB return.', updated)
   }
+  return { userId: upsertedId.toString() }
 }
 
 interface EmailVerification {
+  _id: string
   userId: string
   email: string
-  id: string
+  secret: string
+  createdAt: Date
 }
 
 export async function initiateVerification (userId: string, email: string): Promise<void> {
-  const created = await verificationsContainer.items.create({ userId, email })
-  if (created.resource == null) {
-    throw new AzureError('Unexpected CosmosDB return.', created)
-  }
-
-  const verification = created.resource
+  const secret = (await randomBytesAsync(32)).toString('hex')
+  const verification: Omit<EmailVerification, '_id'> = { userId, email, createdAt: new Date(), secret }
+  const { insertedId } = await verificationCollection.insertOne(verification)
   const verificationEmail: emailClient.MailDataRequired = {
     to: verification.email,
     from: { name: 'Argon Contest Server', email: 'noreply@argoncs.io' },
     subject: 'Please verify your email',
-    html: `User: ${verification.userId}<br>Token: ${verification.id}`
+    html: `User: ${verification.userId}<br>Token: ${insertedId.toString()}${secret}`
   }
 
   await emailClient.send(verificationEmail)
 }
 
-export async function completeVerification (userId: string, verificationId: string): Promise<{userId: string, statusChanged: boolean}> {
-  const userItem = usersContainer.item(userId, userId)
-  const fetchedUser = await userItem.read<User>()
-  if (fetchedUser.resource == null) {
-    throw new NotFoundError('User not found.', { userId })
+export async function completeVerification (userId: string, token: string): Promise<{ userId: string, statusChanged: boolean }> {
+  const user = await userCollection.findOne({ _id: userId })
+  if (user == null) {
+    throw new NotFoundError('User does not exist.', { userId })
   }
-  const user = fetchedUser.resource
   if (user.email === user.verifiedEmail) {
-    return { userId: user.id, statusChanged: false }
+    return { userId: user._id, statusChanged: false }
   }
 
-  const verificationItem = verificationsContainer.item(verificationId, userId)
-  const fetchedVerification = await verificationItem.read<EmailVerification>()
-  if (fetchedVerification.resource == null || fetchedVerification.resource.userId !== user.id) {
-    throw new AuthenticationError('Invalid verification.', { token: verificationId })
+  const id = token.slice(0, 24)
+  const secret = token.slice(24)
+
+  const verification = await verificationCollection.findOne<EmailVerification>({ _id: id })
+  if (verification == null || verification.secret !== secret) {
+    throw new AuthenticationError('Invalid verification.', { userId, token })
   }
-  user.verifiedEmail = fetchedVerification.resource.email
-  const replaced = await userItem.replace(user)
-  if (replaced.resource == null) {
-    throw new AzureError('Unexpected CosmosDB return.', replaced)
-  }
-  await verificationItem.delete()
-  return { userId: replaced.resource.id, statusChanged: true }
+  user.verifiedEmail = verification?.email
+  const { upsertedId } = await userCollection.updateOne({ _id: userId }, { $set: user })
+  return { userId: upsertedId.toString(), statusChanged: true }
 }
 
-export async function authenticateUser (usernameOrEmail: string, password: string): Promise<{userId: string, scopes: Record<string, string[]>, role: UserRole}> {
-  let userIndex = await usernameIndexContainer.item(usernameOrEmail, usernameOrEmail).read<UserIndex>()
-
-  if (userIndex.resource == null) {
-    if (userIndex.statusCode === 404) {
-      userIndex = await emailIndexContainer.item(usernameOrEmail, usernameOrEmail).read<UserIndex>()
-    } else {
-      throw new AzureError('Unexpected CosmosDB return.', userIndex)
-    }
+export async function authenticateUser (usernameOrEmail: string, password: string): Promise<{ userId: string, scopes: Record<string, string[]>, role: UserRole }> {
+  const user = await userCollection.findOne({ $or: [{ username: usernameOrEmail }, { email: usernameOrEmail }] })
+  if (user == null) {
+    throw new AuthenticationError('Authentication failed.', { usernameOrEmail })
   }
 
-  if (userIndex.resource == null) {
-    if (userIndex.statusCode === 404) {
-      throw new AuthenticationError('Authentication failed.', { usernameOrEmail })
-    } else {
-      throw new AzureError('Unexpected CosmosDB return.', userIndex)
-    }
-  }
-
-  const { userId } = userIndex.resource
-  const fetchedUser = await usersContainer.item(userId, userId).read<User>()
-  if (fetchedUser.resource == null) {
-    throw new AzureError('User does not exist but mapping exists.', fetchedUser)
-  }
-
-  const user = fetchedUser.resource
   const hash = (await pbkdf2Async(password, user.password.salt, 100000, 512, 'sha512')).toString('base64')
   if (hash === user.password.hash) {
     if (user.email !== user.verifiedEmail) {
-      throw new AuthorizationError('Please verify your email first.', { userId })
+      throw new AuthorizationError('Please verify your email first.', { userId: user._id })
     }
-    return { userId: user.id, scopes: user.scopes, role: user.role }
+    return { userId: user._id, scopes: user.scopes, role: user.role }
   } else {
     throw new AuthenticationError('Authentication failed.', { usernameOrEmail })
   }
