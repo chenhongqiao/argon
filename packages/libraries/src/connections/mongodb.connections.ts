19b143706bc1797594c2f1aed7fdd1030b8a3ce1
Harry Chen <harrychen0314@gmail.com>
2023-02-20 11:14:43 -0800

    refactor: use db name from mongo url
    Signed-off-by: Harry Chen <harrychen0314@gmail.com>

--- packages/libraries/src/connections/mongodb.connections.ts
+++ packages/libraries/src/connections/mongodb.connections.ts
@@ -3,6 +3,6 @@ import { MongoClient } from 'mongodb'
 const mongoURL = process.env.MONGO_URL ?? ''
 
 export const mongoClient = new MongoClient(mongoURL)
-export const mongoDB = mongoClient.db('argon')
+export const mongoDB = mongoClient.db()
 
 export { IndexSpecification, CreateIndexesOptions, MongoServerError, ObjectId } from 'mongodb'
--- packages/libraries/src/index.ts
+++ packages/libraries/src/index.ts
@@ -1,7 +1,7 @@
 export * from './connections/blobStorage.connections'
 export * from './connections/fileSystem.connections'
 export * from './connections/serviceBus.connections'
-export * from './connections/mongo.connections'
+export * from './connections/mongodb.connections'
 export * from './connections/minio.connections'
 
 export * from './utils/testcase.utils'
