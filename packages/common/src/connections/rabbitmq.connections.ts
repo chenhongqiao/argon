5f910c25b9b19f007c6a7b6992a85ed13e5e53c0
Harry Chen <harrychen0314@gmail.com>
2023-06-05 21:21:11 -0700

    feat: send judger result to rabbitmq
    Signed-off-by: Harry Chen <harrychen0314@gmail.com>

--- packages/common/src/connections/rabbitmq.connections.ts
+++ packages/common/src/connections/rabbitmq.connections.ts
@@ -4,7 +4,9 @@ let rabbitMQ: Channel
 
 export const judgerExchange = 'judger'
 export const judgerTasksQueue = 'judger-tasks'
+export const judgerResultsQueue = 'judger-results'
 export const judgerTasksKey = 'tasks'
+export const judgerResultsKey = 'results'
 
 export async function connectRabbitMQ (): Promise<void> {
   const connection = await amqplib.connect(process.env.RABBITMQ_URL ?? '')
@@ -12,7 +14,9 @@ export async function connectRabbitMQ (): Promise<void> {
 
   await rabbitMQ.assertExchange(judgerExchange, 'direct')
   await rabbitMQ.assertQueue(judgerTasksQueue)
-  await rabbitMQ.bindQueue(judgerTasksQueue, judgerExchange, 'tasks')
+  await rabbitMQ.bindQueue(judgerTasksQueue, judgerExchange, judgerTasksKey)
+  await rabbitMQ.assertQueue(judgerResultsQueue)
+  await rabbitMQ.bindQueue(judgerResultsQueue, judgerExchange, judgerResultsKey)
 }
 
 export { rabbitMQ }
--- packages/judger/src/start.ts
+++ packages/judger/src/start.ts
@@ -2,12 +2,11 @@ import { destroySandbox, initSandbox } from './services/sandbox.services'
 import { gradeSubmission } from './services/grading.services'
 import { compileSubmission } from './services/compile.services'
 
-import { GradingTask, CompilingTask, JudgerTaskType } from '@argoncs/types'
-import { rabbitMQ, judgerTasksQueue } from '@argoncs/common'
+import { GradingTask, CompilingTask, JudgerTaskType, GradingResult, CompilingResult } from '@argoncs/types'
+import { rabbitMQ, judgerTasksQueue, judgerExchange, judgerResultsKey } from '@argoncs/common'
 
 import os = require('node:os')
 import { randomUUID } from 'node:crypto'
-import got from 'got'
 import { pino } from 'pino'
 import Sentry = require('@sentry/node')
 
@@ -17,56 +16,12 @@ const logger = pino()
 
 const availableBoxes = new Set()
 const judgerId = randomUUID()
-const serverBaseURL = process.env.SERVER_BASE_URL ?? 'http://127.0.0.1:8000'
 
 Sentry.init({
   dsn: 'https://54ac76947d434e9a981b7e85191910cc@o1044666.ingest.sentry.io/65541781',
   environment: process.env.NODE_ENV,
   release: version
 })
-
-async function handleGradingTask (task: GradingTask, boxId: number): Promise<void> {
-  await initSandbox(boxId)
-  const result = await gradeSubmission(task, boxId)
-  await destroySandbox(boxId)
-  availableBoxes.add(boxId)
-  try {
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    await got.put(new URL(`/submission-results/${task.submissionId}/testcase-result/${task.testcaseIndex}`, serverBaseURL).href, {
-      json: result,
-      headers: {
-        Authorization: `Bearer ${process.env.JUDGER_TOKEN ?? ''}`
-      },
-      timeout: {
-        request: 30000
-      }
-    })
-    logger.info(task, 'Task completed.')
-  } catch (err) {
-    logger.error(err)
-  }
-}
-
-async function handleCompilingTask (task: CompilingTask, boxId: number): Promise<void> {
-  await initSandbox(boxId)
-  const result = await compileSubmission(task, boxId)
-  await destroySandbox(boxId)
-  try {
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    await got.put(new URL(`/submission-results/${task.submissionId}/compiling-result`, serverBaseURL).href, {
-      json: result,
-      headers: {
-        Authorization: `Bearer ${process.env.JUDGER_TOKEN ?? ''}`
-      },
-      timeout: {
-        request: 30000
-      }
-    })
-    logger.info(task, 'Task completed.')
-  } catch (err) {
-    logger.error(err)
-  }
-}
 
 export async function startJudger (): Promise<void> {
   const cores = os.cpus().length
@@ -89,20 +44,31 @@ export async function startJudger (): Promise<void> {
       try {
         if (availableBoxes.size === 0) {
           rabbitMQ.reject(message, true)
+          logger.info('Received a task when no box is available.')
           return
         }
         const task = JSON.parse(message.content.toString()) as GradingTask | CompilingTask
-        logger.info(task, 'New task received.')
+
+        logger.info(task, 'Processing a new task.')
         availableBoxes.delete(boxId)
+        await initSandbox(boxId)
+
+        let result: GradingResult | CompilingResult
+
         if (task.type === JudgerTaskType.Grading) {
-          await handleGradingTask(task, boxId)
+          result = await gradeSubmission(task, boxId)
         } else if (task.type === JudgerTaskType.Compiling) {
-          await handleCompilingTask(task, boxId)
+          result = await compileSubmission(task, boxId)
+        } else {
+          throw Error('Invalid task type.')
         }
+
+        rabbitMQ.publish(judgerExchange, judgerResultsKey, Buffer.from(JSON.stringify(result)))
       } catch (err) {
         Sentry.captureException(err)
         rabbitMQ.reject(message, false)
       } finally {
+        await destroySandbox(boxId)
         availableBoxes.add(boxId)
       }
     }
