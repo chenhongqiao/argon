32e2a545707f49a583db88227d85866d71b5d3ad
Harry Chen <harrychen0314@gmail.com>
2023-06-07 05:41:53 +0000

    fix(common): services trigger mongo only when called

--- packages/common/src/services/problem.services.ts
+++ packages/common/src/services/problem.services.ts
@@ -2,9 +2,8 @@ import { Problem } from '@argoncs/types'
 import { NotFoundError } from 'http-errors-enhanced'
 import { mongoDB } from '../connections/mongodb.connections.js'
 
-const problemBankCollection = mongoDB.collection<Problem>('problemBank')
-
 export async function fetchFromProblemBank (problemId: string, domainId: string): Promise<Problem> {
+  const problemBankCollection = mongoDB.collection<Problem>('problemBank')
   const problem = await problemBankCollection.findOne({ id: problemId, domainId })
   if (problem == null) {
     throw new NotFoundError('No problem found in this domain with the given ID.', { problemId, domainId })
--- packages/common/src/services/submission.services.ts
+++ packages/common/src/services/submission.services.ts
@@ -2,9 +2,8 @@ import { ContestSubmission, TestingSubmission } from '@argoncs/types'
 import { NotFoundError } from 'http-errors-enhanced'
 import { mongoDB } from '../connections/mongodb.connections.js'
 
-const submissionCollection = mongoDB.collection<TestingSubmission | ContestSubmission>('submissions')
-
 export async function fetchSubmission (submissionId: string): Promise<TestingSubmission | ContestSubmission> {
+  const submissionCollection = mongoDB.collection<TestingSubmission | ContestSubmission>('submissions')
   const submission = await submissionCollection.findOne({ id: submissionId })
   if (submission == null) {
     throw new NotFoundError('No submission found with the given ID.', { submissionId })
