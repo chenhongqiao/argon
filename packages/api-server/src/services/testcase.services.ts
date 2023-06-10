71e1622d69a17fb28cd85095586d1411ffecfdbb
Harry Chen <harrychen0314@gmail.com>
2023-06-10 19:57:42 +0000

    fix(api-server): non existing testcase checker

--- packages/api-server/src/services/testcase.services.ts
+++ packages/api-server/src/services/testcase.services.ts
@@ -5,9 +5,13 @@ import path = require('node:path')
 
 export async function testcaseExists (problemId: string, domainId: string, filename: string, versionId: string): Promise<void> {
   const objectName = path.join(domainId, problemId, filename)
-  const stat = await minio.statObject('testcases', objectName, { versionId })
+  try {
+    const stat = await minio.statObject('testcases', objectName, { versionId })
 
-  if (stat == null || stat.versionId !== versionId) {
+    if (stat == null || stat.versionId !== versionId) {
+      throw new NotFoundError('No test case found with the given identifiers.', { domainId, problemId, filename, versionId })
+    }
+  } catch (err) {
     throw new NotFoundError('No test case found with the given identifiers.', { domainId, problemId, filename, versionId })
   }
 }
