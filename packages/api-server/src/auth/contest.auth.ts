33697aeb5cc250002ff63ce4e9a4d7fc763614fc
Harry Chen <harrychen0314@gmail.com>
2023-06-22 18:47:28 -0700

    feat(api-server): contest registration verifier

--- packages/api-server/src/auth/contest.auth.ts
+++ packages/api-server/src/auth/contest.auth.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { ForbiddenError, InternalServerError, NotFoundError } from 'http-errors-enhanced'
+import { ForbiddenError, InternalServerError, NotFoundError, UnauthorizedError } from 'http-errors-enhanced'
 import { fetchContest } from '../services/contest.services.js'
 
 export async function verifyContestPublished (request: FastifyRequest, reply: FastifyReply) {
-  // @ts-expect-error: url of domain resources always includes domainId as a parameter.
+  // @ts-expect-error: url of contest resources always includes contestId as a parameter.
   const contestId = request.params.contestId
   if (contestId == null || typeof contestId !== 'string') {
     return new InternalServerError('Resource not associated with a contest')
@@ -15,8 +15,26 @@ export async function verifyContestPublished (request: FastifyRequest, reply: Fa
   }
 }
 
+export function verifyContestRegistration (request: FastifyRequest, reply: FastifyReply, done) {
+  if (request.auth == null) {
+    return done(new UnauthorizedError('User not logged in'))
+  }
+
+  // @ts-expect-error: url of contest resources always includes contestId as a parameter.
+  const contestId = request.params.contestId
+  if (contestId == null || typeof contestId !== 'string') {
+    return done(new InternalServerError('Resource not associated with a contest'))
+  }
+
+  if (request.auth.teams[contestId] == null || typeof request.auth.teams[contestId] !== 'string') {
+    return done(new ForbiddenError('Contest registration is required'))
+  }
+
+  done()
+}
+
 export async function verifyContestBegan (request: FastifyRequest, reply: FastifyReply) {
-  // @ts-expect-error: url of domain resources always includes domainId as a parameter.
+  // @ts-expect-error: url of contest resources always includes contestId as a parameter.
   const contestId = request.params.contestId
   if (contestId == null || typeof contestId !== 'string') {
     return new InternalServerError('Resource not associated with a contest')
@@ -29,7 +47,7 @@ export async function verifyContestBegan (request: FastifyRequest, reply: Fastif
 }
 
 export async function verifyContestRunning (request: FastifyRequest, reply: FastifyReply) {
-  // @ts-expect-error: url of domain resources always includes domainId as a parameter.
+  // @ts-expect-error: url of contest resources always includes contestId as a parameter.
   const contestId = request.params.contestId
   if (contestId == null || typeof contestId !== 'string') {
     return new InternalServerError('Resource not associated with a contest')
